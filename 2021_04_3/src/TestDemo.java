7ae42f22db5ac14e567c0157b7403e4d312e5c91
qkj <2660015902@qq.com>
2021-11-04 22:37:54 +0800

    测试提交

---
+++ 2021_04_3/src/TestDemo.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Qkj
+ * Date: 2021-11-04
+ * Time: 16:11
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(11);
+        System.out.println("HelloWorld");
+    }
+}
