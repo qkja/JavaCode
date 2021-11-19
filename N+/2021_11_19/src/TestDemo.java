4766f90c2b6cb2e88240e822dd677071a9e8fddd
qkja <2660015902@qq.com>
2021-11-19 23:45:32 +0800

    牛客网

---
+++ N+/2021_11_19/src/TestDemo.java
@@ -0,0 +1,66 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Qkj
+ * Date: 2021-11-19
+ * Time: 20:12
+ */
+
+import java.util.Scanner;
+public class TestDemo {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        //write your code here......
+        while(scanner.nextInt() > 0) {
+
+            count++;
+        }
+        System.out.println(count);
+
+//        int num = 9;
+//        int i = 9;
+//        int count = 1;
+//        long sum = 9L;
+//        while(i > 0) {
+//
+//            count = count * 10 + 1;
+//            //1 11
+//            sum += num * count;
+//            i--;
+//        }
+//        System.out.println(sum);
+        //System.out.println(count);
+        //输入还不太了解
+
+//        Scanner scanner = new Scanner(System.in);
+//        String grade =scanner.next();
+//        char score = scannerChar.next();
+//
+//        if(score == 'A') {
+//
+//            System.out.println("You are right");
+//        }
+
+
+//        if(grade.equals("A")) {
+//
+//            System.out.println("优秀");
+//        } else if(grade.equals("B")) {
+//
+//            System.out.println("良好");
+//        } else if(grade.equals("C")) {
+//
+//            System.out.println("及格");
+//        } else if(grade.equals("D")) {
+//
+//            System.out.println("不及格");
+//        } else {
+//
+//            System.out.println("未知等级");
+//        }
+    }
+}
