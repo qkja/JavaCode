15e499d42c390ab8d7425bf8d0e37884230484b7
qkja <2660015902@qq.com>
2021-11-20 12:14:09 +0800

    初识javaSE博客补充

---
+++ 2021_11_20/TestDemo.java
@@ -0,0 +1,21 @@
+public class TestDemo {
+	
+	public static void main(String[] args) {
+		
+		boolean flg1 = true;
+		boolean flg2 = false;
+		if(flg1) {
+			
+			System.out.println("Good");
+		}
+		
+		if(!flg2) {
+			
+			System.out.println("Very Good");
+		}
+		
+		
+		// System.out.println(Char.MAX_VALUE);
+		// System.out.println(Char.MIN_VALUE);
+	}
+}
