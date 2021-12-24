bc26c574cafb9a45471e89d6aef456fd5712345e
algorithmic-jacko <qkj1205@163.com>
2021-12-24 22:05:40 +0800

    继承博客+this

--- 2021_12_23/src/TestDemo.java
+++ 2021_12_23/src/TestDemo.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -6,22 +8,49 @@
  * Time: 22:25
  */
 
-class Animal {
-    public String name;
+//class Animal {
+//    public String name;
+//    public Animal(String name) {
+//
+//        this.name = name;
+//    }
+//
+//
+//    private void eat() {
+//
+//        System.out.println(this.name + " eat()");
+//    }
+//
+//    public void sleep() {
+//
+//        System.out.println(this.name + " sleep()");
+//    }
+//}
 
-    private void eat() {
-        System.out.println(this.name + " eat()");
-    }
+//class Animal {
+//    public String name;
+//
+//    public Animal(String name) {
+//
+//        this.name = name;
+//    }
+//    public void sleep() {
+//
+//        System.out.println(this.name + " sleep()");
+//    }
+//
+//}
+
+class Animal {
+    protected String name;
     public void sleep() {
         System.out.println(this.name + " sleep()");
     }
 }
 
 class Cat extends Animal {
-    public String name;
-    public void sleep() {
-        System.out.println("sleep()!!!");
-    }
+
+
 }
 
 public class TestDemo {
@@ -30,12 +59,37 @@ public class TestDemo {
         Cat cat = new Cat();
         cat.name = "mimi";
         cat.sleep();
+        //cat.fun();
     }
 }
 
-class Bird  extends Animal{
+//class Cat extends Animal {
+//
+//    public Cat(int data) {
+//        super.sleep();
+//        super.age = data;
+//    }
+//}
+//class Cat extends Animal {
+//    public String name;
+//    public void sleep() {
+//        System.out.println("sleep()!!!");
+//    }
+//}
 
-}
+//public class TestDemo {
+//    public static void main(String[] args) {
+//
+//        Cat cat = new Cat("mimi");
+//        cat.sleep();
+////        cat.name = "mimi";
+////        cat.sleep();
+//    }
+//}
+
+//class Bird  extends Animal{
+//
+//}
 
 
 //class Cat extends Animal {
