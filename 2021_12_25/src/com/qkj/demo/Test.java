fec00bf976dd770cc89fec06d697a13a4e43af93
algorithmic-jacko <qkj1205@163.com>
2021-12-25 21:17:13 +0800

    继承博客+this

---
+++ 2021_12_25/src/com/qkj/demo/Test.java
@@ -0,0 +1,21 @@
+package com.qkj.demo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Qkj
+ * Date: 2021-12-25
+ * Time: 20:35
+ */
+class  a {
+    public int b;
+}
+class b extends a {
+    public void func() {
+        super.b = 10;
+    }
+}
+public class Test {
+    protected  int a;
+}
+
