97cc7da43f7873a4afe4ca7b93381b5f7d3b3fbe
algorithmic-jacko <qkj1205@163.com>
2021-12-28 23:52:43 +0800

    多态

--- 2021_12_28/src/TestDemo.java
+++ 2021_12_28/src/TestDemo.java
@@ -1,8 +1,16 @@
-package PACKAGE_NAME;/**
-* Created with IntelliJ IDEA.
-* Description:
-* User: Qkj
-* Date: 2021-12-28
-* Time: 23:18
-*/public class TestDemo {
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Qkj
+ * Date: 2021-12-28
+ * Time: 23:18
+ */
+
+class Test{
+}
+
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
 }
