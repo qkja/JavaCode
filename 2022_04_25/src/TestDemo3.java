87b3d0d2d87ca5e88e228e3d59b464a74200cec0
algorithmic-jacko <qkj1205@163.com>
2022-04-25 23:15:20 +0800

    map

---
+++ 2022_04_25/src/TestDemo3.java
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Qkj
+ * Date: 2022-04-25
+ * Time: 16:28
+ */
+public class TestDemo3 {
+    //public static int n;
+
+    public void func() {
+        //static final int b = 10;
+        final int b = 10;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
