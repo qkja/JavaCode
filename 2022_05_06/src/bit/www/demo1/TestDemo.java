72355299f43c1ad31b567960df0546cdf0fd70ba
algorithmic-jacko <qkj1205@163.com>
2022-05-06 20:20:05 +0800

    hash

---
+++ 2022_05_06/src/bit/www/demo1/TestDemo.java
@@ -0,0 +1,32 @@
+package bit.www.demo1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Qkj
+ * Date: 2022-05-06
+ * Time: 16:56
+ */
+
+//class Solution {
+//    public int firstUniqChar(String s) {
+//        if(s == null) {
+//            return -1;
+//        }
+//        int[] array = new int[26];
+//        for(int i=0;i<s.length();i++) {
+//            int ret = s.charAt(i) - 'a';
+//            array[ret]++;
+//        }
+//        for (int i = 0; i < s.length(); i++) {
+//            if(array[(int)(s.charAt(i) - 'a')] == 1)  {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+//}
+
+
+public class TestDemo {
+}
